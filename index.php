17bc3d6f2fe34caee1f270e984ad56041fca1576
rahul-singh <rahulas5@gmail.com>
2015-03-13 14:57:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,3 @@
 <?php
-
-echo "<pre>";
-$res = array();
-//get details from last commit
-exec("git log --name-only --max-count=4", $res);
-
-//keep only files names, not commit details
-foreach($res as $k=>$string)
-{
-	if(!is_file($string))
-		unset($res[$k]);
-}
-
-//recount array
-array_values($res);
-
-echo "<h1>Files found in git log result:</h1>";
-print_r($res);
-echo "<hr />";
-
+echo 'test';
+?>
